fd2069906b804dfe174709917628a64f614eeff4
jbaresfe <jbaresfe@redhat.com>
2020-11-01 14:37:32 +0100

    add choco build package and choco install package
    adding gitignore
    
    chocolateyinstall script

---
+++ roles/choco_package_build/files/generic_setup/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿# stop on all errors
+$ErrorActionPreference = 'Stop'
+# source parameter file
+$parameterfile = $PSScriptRoot + "\params.ps1"
+. $parameterfile
+$Argu="-packageName $packageName"
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir $packageExecutable
+
+$packageArgs = @{
+  installerType  = 'exe'
+  file           = $fileLocation
+  silentArgs     = "/S"
+  validExitCodes = @(0,3010,1604)
+}
+
+Install-ChocolateyPackage @packageArgs $Argu
